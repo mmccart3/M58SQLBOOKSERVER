3ab23f9b9225bd306550499a743900a01e936ed3
Mark McCarthy <mm3guides@gmail.com>
2024-10-16 14:41:32 +0100

    bookroutes corrected

--- src/db/routes/bookRoutes.js
+++ src/db/routes/bookRoutes.js
@@ -2,19 +2,19 @@ const {Router} = require("express");
 const bookRouter = Router();
 
 // import a function to add a book = addBook.js
-const {addBook} = require("../controllers/addBook");
+const addBook = require("../controllers/addBook");
 // import a function to list all books = listAllBooks.js
-const {listAllBooks} = require("../controllers/listAllBooks");
+const listAllBooks = require("../controllers/listAllBooks");
 // import a function to update the author name on a specific book = updateAuthor.js
-const {updateAuthor} = require("../controllers/updateAuthor");
+const updateAuthor = require("../controllers/updateAuthor");
 // import a function to update the genre of a book = updateGenre.js
-const {updateGenre} = require("../controllers/updateGenre");
+const updateGenre = require("../controllers/updateGenre");
 // import a function to delete a book = deleteBook.js
-const {deleteBook} = require("../controllers/deleteBook");
+const deleteBook = require("../controllers/deleteBook");
 // import a function to list the details of an individual book = bookDetails.js
-const {bookDetails} = require("../controllers/bookDetails");
+const bookDetails = require("../controllers/bookDetails");
 // import a function to delete all books = deleteAllBooks.js
-const {deleteAllBooks} = require("../controllers/deleteAllBooks");
+const deleteAllBooks = require("../controllers/deleteAllBooks");
 
 bookRouter.post("/addBook",addBook);
 bookRouter.get("/listAllBooks",listAllBooks);
